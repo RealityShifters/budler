2a1a1617386114528fbb4b3607816b3584ae34b1
Jarrett Yeap <hooitongx@gmail.com>
2020-05-15 13:04:23 +0800

    🔧 Use typescript for all components and pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Input, Row, Col } from "antd"
+import { Col, Input, Row } from "antd"
 import React from "react"
 import Layout from "../components/layout"
 
@@ -8,7 +8,7 @@ const IndexPage = () => (
   <Layout>
     <Row>
       <Col span={12}>
-        <TextArea rows="5" />
+        <TextArea rows={5} />
       </Col>
       <Col span={12}></Col>
     </Row>
