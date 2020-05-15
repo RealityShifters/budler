960522124f5a6c4ad38c88c5f6bb459a4f84ddc9
Jarrett Yeap <hooitongx@gmail.com>
2020-05-15 12:34:17 +0800

    🚀 First Commit

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import { Input, Row, Col } from "antd"
+import React from "react"
+import Layout from "../components/layout"
+
+const { TextArea } = Input
+
+const IndexPage = () => (
+  <Layout>
+    <Row>
+      <Col span={12}>
+        <TextArea rows="5" />
+      </Col>
+      <Col span={12}></Col>
+    </Row>
+  </Layout>
+)
+
+export default IndexPage
